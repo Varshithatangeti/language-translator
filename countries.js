9be0fb2eebd418e6b88948adfdc6d07491cd3dac
Varshithatangeti <142086561+Varshithatangeti@users.noreply.github.com>
2024-03-19 22:41:25 +0530

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,10 @@
+const countries = {
+    "bn-IN": "Bengali",
+    "en-GB": "English",
+    "gu-IN": "Gujarati",
+    "hi-IN": "Hindi",
+    "kn-IN": "Kannada",
+    "pa-IN": "Panjabi",
+    "te-IN": "Telugu",
+    "es-ES": "Spanish",
+}
